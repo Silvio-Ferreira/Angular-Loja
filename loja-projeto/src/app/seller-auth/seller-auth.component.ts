84fdfdbc9366c56517ce603fc948e1826e09e6af
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2025-02-10 11:07:14 -0300

    Update
    Nav e rotas para home e seller pages criadas

---
+++ loja-projeto/src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  standalone: false,
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+}
