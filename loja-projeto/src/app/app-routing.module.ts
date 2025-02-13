cbc18db60b273be90f3606651b51b143e987178f
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2025-02-13 17:10:14 -0300

    cadastro de produtos
    Conseguimos cadastrar os produtos e salvar no nosso db.json

--- loja-projeto/src/app/app-routing.module.ts
+++ loja-projeto/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { authGuard } from './auth.guard';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,11 @@ const routes: Routes = [
     path:'seller-home',
     canActivate:[authGuard]
 
+  },
+  {
+    component:SellerAddProductComponent,
+    path:'seller-add-product',
+    canActivate:[authGuard]
   }
 ];
 
---
+++ loja-projeto/src/app/seller-add-product/seller-add-product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-add-product',
+  standalone: false,
+  templateUrl: './seller-add-product.component.html',
+  styleUrl: './seller-add-product.component.css'
+})
+export class SellerAddProductComponent {
+
+  constructor(){ }
+
+  ngOninit():void {
+
+  }
+
+  submit(data:object){
+
+  }
+}
