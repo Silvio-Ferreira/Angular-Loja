54c1335970d9197aa09541ff430413c79a9ac698
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2025-02-27 19:09:11 -0300

    fix

--- loja-projeto/src/app/home/home.component.ts
+++ loja-projeto/src/app/home/home.component.ts
@@ -3,16 +3,12 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { product } from '../data-types';
 import { ProductService } from '../services/product.service';
 
-
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-
-
-
 export class HomeComponent implements OnInit {
   popularProducts:undefined | product[]
 
