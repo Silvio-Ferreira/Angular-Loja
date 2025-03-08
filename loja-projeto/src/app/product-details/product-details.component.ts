c27f8577226d987bdd5645a324e58f7671ed9063
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2025-03-08 11:36:01 -0300

    Update
    Página de detalhes dos produtos criada, puxando os dados dinamicamente.

---
+++ loja-projeto/src/app/product-details/product-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../data-types';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: false,
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent implements OnInit{
+  productData: undefined | product;
+
+  constructor(private activeRoute:ActivatedRoute, private product: ProductService) { }
+
+  ngOnInit(): void {
+    let productId = this.activeRoute.snapshot.paramMap.get('productId');
+    productId && this.product.getProduct(productId).subscribe((result)=>{
+      this.productData = result
+    })
+  }
+}
